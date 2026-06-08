4748b8be0e2a89c215af188d9d29da0e7ce3f657
Ali Haidar <ecommerceedittor@gmail.com>
2026-06-08 23:39:59 +0500

    contact form issue resolve

--- newsletter/admin.py
+++ newsletter/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
-from .models import Subscriber
+from .models import Subscriber, ContactMessage
+
 
 @admin.register(Subscriber)
 class SubscriberAdmin(admin.ModelAdmin):
     list_display = ("email", "subscribed_at")
     search_fields = ("email",)
     ordering = ("-subscribed_at",)
+
+
+@admin.register(ContactMessage)
+class ContactMessageAdmin(admin.ModelAdmin):
+    list_display = ("name", "email", "topic", "created_at") 
+    list_filter = ("topic", "created_at")             
+    search_fields = ("name", "email", "message")          
